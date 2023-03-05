008e28967770977285025e6d6c58b48638a80042
Jonathan Alvarado <wizardx1407@gmail.com>
2023-03-05 02:55:34 -0400

    feat: Add responsive dashboard design

--- src/Components/EarningOverview.tsx
+++ src/Components/EarningOverview.tsx
@@ -1,30 +1,31 @@
 import Image from "next/image";
-import salesIcon from "../assets/img/sales-icon.svg";
+import salesIconOne from "../assets/img/sales-icon-1.svg";
+import salesIconTwo from "../assets/img/sales-icon-2.svg";
 
 function EarningOverview() {
   return (
-    <div className="mb-7 rounded-lg bg-white px-6 pt-4 pb-8">
+    <article className="mb-7 rounded-lg bg-white px-6 pt-4 pb-8 xl:m-0">
       <p className="mb-3.5 h-12 text-sm">
         In your zips over the last 18 months, we’ve originated:
       </p>
       <div className="columns-2">
-        <div className="flex flex-col">
-          <Image className="mb-3" src={salesIcon} alt="Sales icon" />
+        <div className="flex flex-col xl:pl-4">
+          <Image className="mb-3" src={salesIconOne} alt="Sales icon" />
           <span className="mb-1.5 text-3xl">$20M</span>
           <small className="text-xs text-sold-gray-1">in sales</small>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col sm:pl-4">
           {/**
            * Todo change to the proper icon
            */}
-          <Image className="mb-3" src={salesIcon} alt="Sales icon" />
+          <Image className="mb-3" src={salesIconTwo} alt="Sales icon" />
           <span className="mb-1.5 text-3xl">$500K</span>
           <small className="text-xs text-sold-gray-1">
             in Commissions (est)
           </small>
         </div>
       </div>
-    </div>
+    </article>
   );
 }
 
--- src/Components/RankingCard.tsx
+++ src/Components/RankingCard.tsx
@@ -3,31 +3,35 @@ import arrowRoundDownIcon from "../assets/img/arrow-round-down-icon.svg";
 
 function RankingCard() {
   return (
-    <div className="px-4 ">
-      <div className="-mt-96 mb-6 rounded-lg bg-white p-6 shadow-md">
-        <p className="mb-3 text-base">
-          Our referrals are routed to the top performing agents in our network.
-          Rankings are based on numerous factors, including:
-        </p>
-        <ul className="mb-8 list-inside pl-2 text-sm">
-          <li>
-            <span className="text-sold-orange-0">+</span> Transaction statistics
-          </li>
-          <li>
-            <span className="text-sold-orange-0">+</span> Responsiveness
-          </li>
-          <li>
-            <span className="text-sold-orange-0">+</span> Customer Satisfaction{" "}
-          </li>
-          <li>
-            <span className="text-sold-orange-0">+</span> Portal Updates
-          </li>
-        </ul>
+    <div className="mx-auto px-4 xl:w-[70rem] xl:p-0">
+      <div className="-mt-96 mb-6 rounded-lg bg-white p-6 shadow-md xl:-mt-28 xl:mb-12 xl:flex xl:gap-20">
+        <div>
+          <p className="mb-3 text-base xl:w-[27rem]">
+            Our referrals are routed to the top performing agents in our
+            network. Rankings are based on numerous factors, including:
+          </p>
+          <ul className="mb-8 pl-2 text-sm xl:columns-2 xl:p-0">
+            <li>
+              <span className="text-sold-orange-0">+</span> Transaction
+              statistics
+            </li>
+            <li>
+              <span className="text-sold-orange-0">+</span> Responsiveness
+            </li>
+            <li>
+              <span className="text-sold-orange-0">+</span> Customer
+              Satisfaction
+            </li>
+            <li>
+              <span className="text-sold-orange-0">+</span> Portal Updates
+            </li>
+          </ul>
+        </div>
         <div className="bg-sold-gray-2 p-5">
-          <div className="mb-3 flex">
-            <p className="w-52 text-lg font-bold">
+          <div className="mb-3 flex xl:gap-5">
+            <strong className="w-60 text-lg">
               Your average rank in the markets you serve is:
-            </p>
+            </strong>
             <div className="flex items-center">
               <span className="text-4xl text-sold-orange-1">25</span>
               <Image
@@ -36,15 +40,23 @@ function RankingCard() {
                 alt="Arrow round down icon"
               />
             </div>
+            <button
+              className="ml-auto hidden rounded bg-sold-orange-0 p-3 text-base font-bold text-white xl:inline"
+              type="button"
+            >
+              Learn More
+            </button>
           </div>
-          <p className="mb-4 pr-1 text-xs text-sold-gray-3">
+          <p className="mb-4 pr-1 text-xs text-sold-gray-3 xl:w-[30rem]">
             Your ranking will directly affect the number of referrals you
             receive. Learn about our{" "}
-            <span className="text-sold-orange-0">Guaranteed Display</span>{" "}
+            <em className="not-italic text-sold-orange-0">
+              Guaranteed Display
+            </em>{" "}
             program to help improve your ranking.
           </p>
           <button
-            className="w-full rounded bg-sold-orange-0 py-3.5 text-base font-bold text-white"
+            className="w-full rounded bg-sold-orange-0 py-3.5 text-base font-bold text-white xl:hidden"
             type="button"
           >
             Learn More
